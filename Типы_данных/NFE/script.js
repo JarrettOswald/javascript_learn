c37f6796d26206ab2b67688ebc6940d32ad25557
John Doe <johndoe@example.com>
2022-02-04 14:48:48 +0400

    add

--- Типы_данных/NFE/script.js
+++ Типы_данных/NFE/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 
-const sayHi = function(){
+const sayHi = function () {
     console.log("Hello!");
 };
 
@@ -9,8 +9,22 @@ const sayHi = function(){
 console.log(sayHi.name); // Контекстное имя
 
 
-const sayWord = function(word, ...more){
+const sayWord = function (word, ...more) {
     console.log(word);
 };
 
-console.log(sayWord.length); //«length» содержит количество параметров функции
+
+function sayHello() {
+    console.log('Hello!');
+
+    sayHello.count++;
+}
+
+sayHello.count = 0;
+
+
+let sayHi2 = function func(who) {
+    console.log(`Hello, ${who}`);
+};
+
+sayHi2("Name");
---
+++ Типы_данных/new_Function/script.js
@@ -0,0 +1,6 @@
+'use strcit';
+
+
+const sum = new Function('a', 'b', 'return a + b');
+
+console.log(sum(5,7));
